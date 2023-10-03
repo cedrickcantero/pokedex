7d3faf3d50e2c7cc1c3015fe697dcdcaad5ed867
Cedrick Cantero <cedys02@gmail.com>
2023-10-04 03:32:42 +0800

    update the move controller

--- core-app/src/utils/string-helper.js
+++ core-app/src/utils/string-helper.js
@@ -1,3 +1,6 @@
 export const capitalizeFirstChar = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+    return str
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 };
