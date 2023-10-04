7928edda41b45896a93ef21ca255346a7548afa9
Cedrick Cantero <cedys02@gmail.com>
2023-10-04 09:41:40 +0800

    add flex to each column headers

--- core-app/src/components/moves/defaults.js
+++ core-app/src/components/moves/defaults.js
@@ -1,26 +1,8 @@
 export const moveColumnDefs = [
-  { headerName: "ID", field: "_id" },
-  { headerName: "Name", field: "ename" },
-  { headerName: "Type", field: "type" },
-  { headerName: "Accuracy", field: "accuracy" },
-  { headerName: "Power", field: "power" },
-  { headerName: "PP", field: "pp" },
+  { headerName: "ID", field: "_id", flex:1  },
+  { headerName: "Name", field: "ename", flex:1  },
+  { headerName: "Type", field: "type", flex:1  },
+  { headerName: "Accuracy", field: "accuracy", flex:1  },
+  { headerName: "Power", field: "power", flex:1  },
+  { headerName: "PP", field: "pp", flex:1  },
 ];
-
-export const moveTypes = [
-  { id: 'normal', name: 'Normal'},
-  { id: 'fighting', name: 'Fighting'},
-  { id: 'fire', name: 'Fire'},
-  { id: 'ice', name: 'Ice'},
-  { id: 'electric', name: 'Electric'},
-  { id: 'grass', name: 'Grass'},
-  { id: 'poison', name: 'Poison'},
-  { id: 'bug', name: 'Bug'},
-  { id: 'dragon', name: 'Dragon'},
-  { id: 'physic', name: 'Physic'},
-  { id: 'ghost', name: 'Ghost'},
-  { id: 'ground', name: 'Ground'},
-  { id: 'fairy', name: 'Fairy'},
-  { id: 'dark', name: 'Dark'},
-]
-  
